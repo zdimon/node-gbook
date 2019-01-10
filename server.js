58e0cded2504c7d2b0375c3ae3abc45825962e26
zdimon <zdimon@example.com>
2019-01-10 14:51:48 +0200

    mustache tpl

--- server.js
+++ server.js
@@ -3,8 +3,30 @@ var express = require('express');
 var fs = require("fs");
 var mustacheExpress = require('mustache-express');
 
-var app = express(); // приложение...
+var app = express();
 
+// change extention
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+
+
+app.get('/', function (req, res) {
+   res.render('index.html', {'name': 'Dima',
+   
+    'messages': [ 
+           {'name': 'Dima', 
+            'message': 'Hello everyone!'
+           },
+           {'name': 'Vova', 
+            'message': 'Hello everyone from Vova!'
+           }
+     ]
+   
+   })
+})
 
 var server = app.listen(8888, function () {
    console.log("I am running on 8888 port!")
